2dd12a48bf7e4c9b78007f001b6ca14dda7de522
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2024-11-22 09:28:14 +0200

    Remove unused code and specifiy position of list item markers

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,20 @@ import './App.css'
 function App() {
   const [posts, setPosts] = useState([])
 
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(res => res.json())
+      .then(data => setPosts(data))
+  }, [])
+
+ // console.log(posts)
   return (
-
-
     <>
-     
+     <ol>
+        {posts.map((post) => (
+          <li key={post.id}>{`${post.title}`}<br/>{`${post.body}`}</li>
+        ))}
+      </ol>
     </>
   )
 }
