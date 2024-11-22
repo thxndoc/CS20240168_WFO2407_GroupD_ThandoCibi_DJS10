971ce1a0db2542657181a8ab0d7389eddbcc29f4
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2024-11-22 12:45:55 +0200

    setup conditional rendering based on errors

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,45 @@ import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [posts, setPosts] = useState([])
+    const [posts, setPosts] = useState([])
+    const [error, setError] = useState(null)
 
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then(response => response.json())
-      .then(data => setPosts(data))
-  }, [])
+    useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/pots')
+        .then(response => {
+        if (!response.ok) {
+            setError('Data fetching failed')
+            return [];
+        }
+        return response.json()
+        })
+        .then(data => setPosts(data))
+    }, [])
 
- // console.log(posts)
-  return (
+    // console.log(posts)
+    if (error) {
+    return (
+        <>
+            <h1>
+                {error}
+            </h1>
+        </>
+    )
+    }
+    
+    return (
     <>
-    <h1>Posts</h1>
-     <ol>
+        <h1>Posts</h1>
+        <ol>
         {posts.map((post) => (
-          <li key={post.id}>
+            <li key={post.id}>
             {`${post.title}`}<br/>
             <span>{`${post.body}`}</span>
             </li>
         ))}
-      </ol>
+        </ol>
     </>
-  )
+    )
 }
 
 export default App
